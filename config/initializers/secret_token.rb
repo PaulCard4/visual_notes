97621d12b49dd1be6ec07f745d9ca6818de69777
PPC4 <paulcard4@icloud.com>
2013-10-06 20:56:41 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VisualNotes::Application.config.secret_key_base = '2b0d40d48f7e5d4dea7c3414744a3c7c7731fd9569f9102c4592ffb500d34fde41efdf9fce2b5c333a4373bf156137e087176dc67c76000221dcec9eb49f02b5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+VisualNotes::Application.config.session_store :cookie_store, key: '_visual_notes_session'
